07d98afb843a2aa66de7efc0e3a3c50700972e2d
velcrostrap <147015258+velcrostrap@users.noreply.github.com>
2023-10-05 03:30:54 +0100

    Add files via upload

---
+++ Blocksignin.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");var styles = {
+
+///* Google: Sign in with Google */ div#credential_picker_container > iframe[src="https://accounts.google.com/"], iframe#credential_picker_iframe[src="https://accounts.google.com/"]
+    googlesigninwithgoogledivCredentialPickerContaineriframeSrcHttpsAccountsGoogleComElement: {
+            display: "none !important"
+    }
+    iframeCredentialPickerIframeSrcHttpsAccountsGoogleComElement: {
+            display: "none !important"
+    },
+///* Google: Google recommends using Chrome */ div[data-hveid="1"] > div:nth-child(1) iframe[role="presentation"][name="callout"]
+    googlegooglerecommendsusingchromedivDataHveid1divNthChild1iframeRolePresentationNameCalloutElement: {
+            display: "none !important"
+    },
+///* YouTube: Promo*/ yt-mealbar-promo-renderer[has-full-height-image]
+    youTubepromoytMealbarPromoRendererHasFullHeightImageElement: {
+            display: "none !important"
+    }
+}
